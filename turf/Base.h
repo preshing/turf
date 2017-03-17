958a81910942fb89c9af3d88f9d6690bf6ece996
Jeff Preshing <filter-github@preshing.com>
2017-03-17 07:52:40 -0400

    Add Base.h

---
+++ turf/Base.h
@@ -0,0 +1,30 @@
+/*------------------------------------------------------------------------
+  Turf: Configurable C++ platform adapter
+  Copyright (c) 2017 Jeff Preshing
+
+  Distributed under the Simplified BSD License.
+  Original location: https://github.com/preshing/turf
+
+  This software is distributed WITHOUT ANY WARRANTY; without even the
+  implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+  See the LICENSE file for more information.
+------------------------------------------------------------------------*/
+
+#ifndef TURF_BASE_H
+#define TURF_BASE_H
+
+#include <turf/Assert.h>
+#include <turf/Atomic.h>
+#include <turf/ConditionVariable.h>
+#include <turf/CPUTimer.h>
+#include <turf/Heap.h>
+#include <turf/ManualResetEvent.h>
+#include <turf/Mutex.h>
+#include <turf/RaceDetector.h>
+#include <turf/RWLock.h>
+#include <turf/Semaphore.h>
+#include <turf/Thread.h>
+#include <turf/TID.h>
+#include <turf/Util.h>
+
+#endif // TURF_BASE_H
